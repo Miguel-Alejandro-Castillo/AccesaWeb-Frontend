2bf037e41addbabdcf006c11977e274b4996dd09
Miguel Castillo <miguelcastillo1992lp@gmail.com>
2021-08-20 22:48:56 -0300

    Formulario resize text

--- src/services/settings/font-size.js
+++ src/services/settings/font-size.js
@@ -1,3 +1,53 @@
+import React from 'react';
+import $ from 'jquery';
+import _ from 'lodash';
+
+const propertySettingLocalStorage = 'input.name.resizeText';
+
+function Form({valueResizeText, onChangeResizeText}) {
+  return (
+    <p className='box'>
+      <input type='range' width='50' min='0.5' max='1.5' step='0.1' className='_not-focuseable-element' value={valueResizeText} onChange={e => onChangeResizeText(e.target.value)}/>
+    </p>
+  );
+}
+
+
+Form.propTypes = {
+  onChangeResizeText: React.PropTypes.func.isRequired,
+  valueResizeText: React.PropTypes.string.isRequired
+};
+
+function setResizeText(value) {
+  localStorage.setItem(propertySettingLocalStorage, value);
+  document.dispatchEvent(new Event('changeInput'));
+}
+
+function valueResizeText() {
+  return localStorage.getItem(propertySettingLocalStorage) ? localStorage.getItem(propertySettingLocalStorage) : '1.0';
+}
+
+function ResizeTextFunction() {
+  return (
+    <Form valueResizeText={valueResizeText()} onChangeResizeText={setResizeText} />
+  );
+}
+
+//Habria que hacerlo mas performante
+function action(valueSetting) {
+  $('*').each(function() {
+    const element = $(this);
+    const fontSizePx = element.css('font-size');
+    if (!_.isEmpty(fontSizePx) && !_.isEmpty(valueSetting)) {
+      if (_.isEmpty(element.data('origFontSize'))) {
+        element.data('origFontSize', fontSizePx);// Guardar el font size original
+      }
+      const fontSize = parseFloat(element.data('origFontSize')) * parseFloat(valueSetting);
+      element.css('font-size', fontSize);
+    }
+  });
+}
+
 export default {
   name: 'i18n-name',
   description: 'i18n-description',
@@ -12,5 +62,7 @@ export default {
       'description': 'Cambia el tamaño de fuente'
     }
   },
-  contexts: []
+  contexts: [{ functionComponent: ResizeTextFunction }],
+  propertySettingLocalStorage: propertySettingLocalStorage,
+  action: action
 };
