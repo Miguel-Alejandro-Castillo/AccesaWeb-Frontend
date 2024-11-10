3010af2a82490eaeec5ed4524c92a2d8186867e1
Miguel Alejandro Castillo <miguelcastillo1992lp@gmail.com>
2024-11-10 18:42:54 -0300

    Agregar mas actions para modulos de Formato, Personalizar y Accesibilidad

--- src/actions/content_actions.js
+++ src/actions/content_actions.js
@@ -1,9 +1,143 @@
 import $ from 'jquery';
 
-const  socialNetworks = [ 'facebook', 'twitter', 'whatsapp', 'youtube', 'instagram', 'telegram', 'linkedin', 'qzone', 'pinterest', 'badoo', 'tiktok', 'github' ];
+const socialNetworks = ['facebook', 'twitter', 'whatsapp', 'youtube', 'instagram', 'telegram', 'linkedin', 'qzone', 'pinterest', 'badoo', 'tiktok', 'github'];
 
 function showHideImages(param) {
   param ? showImages() : hideImages();
+}
+
+function showHideAds(param) {
+  param ? showAds() : hideAds();
+}
+
+function showHideSocialNetworks(param) {
+  param ? showSocialNetworks() : hideSocialNetworks();
+}
+
+// Función para cambiar el alineamiento de texto en todos los elementos que contienen texto
+function changeTextAlign(param) {
+  // Seleccionar todos los elementos
+  $('*').filter(function() {
+    // Verificar si el elemento contiene texto no vacío
+    return $(this).text().trim().length > 0;
+  }).each(function() {
+    // Almacenar el valor original de text-align si no está ya almacenado
+    if (!$(this).data('original-text-align')) {
+      $(this).data('original-text-align', $(this).css('text-align'));
+    }
+    // Cambiar el alineamiento de texto
+    $(this).css('text-align', param);
+  });
+}
+
+// Función para cambiar el tipo de fuente en todos los elementos que contienen texto
+function changeFont(param) {
+  // Seleccionar todos los elementos
+  $('*').filter(function() {
+    // Verificar si el elemento contiene texto no vacío
+    return $(this).text().trim().length > 0;
+  }).each(function() {
+    // Almacenar el valor original de font-family si no está ya almacenado
+    if (!$(this).data('original-font-family')) {
+      $(this).data('original-font-family', $(this).css('font-family'));
+    }
+    // Cambiar el tipo de fuente
+    $(this).css('font-family', param);
+  });
+}
+
+function changeFontSize(param) {
+  $('*').filter(function() {
+    return $(this).text().trim().length > 0;
+  }).each(function() {
+    if (!$(this).data('original-font-size')) {
+      $(this).data('original-font-size', $(this).css('font-size'));
+    }
+    $(this).css('font-size', param + 'rem');
+    /*
+    $(this).css({
+      'overflow': 'hidden',
+      'text-overflow': 'ellipsis',
+      'white-space': 'nowrap',
+      'word-wrap': 'break-word'
+    });
+    */
+  });
+}
+
+function changeLineSpacing(param) {
+  $('body,p,span,div,main,article,section,header,footer,aside,nav,h1,h2,h3,h4,h5,h6').filter(function() {
+    return $(this).text().trim().length > 0;
+  }).each(function() {
+    const elem = $(this);
+    if (!elem.data('original-line-height')) {
+      elem.data('original-line-height', elem.css('line-height'));
+    }
+    elem.css('line-height', param === '*' ? elem.data('original-line-height') : param);
+  });
+}
+
+function changeParagraphSpacing(param) {
+  $('p').filter(function() {
+    return $(this).text().trim().length > 0;
+  }).each(function() {
+    const elem = $(this);
+    if (!elem.data('original-margin-bottom')) {
+      elem.data('original-margin-bottom', elem.css('margin-bottom'));
+    }
+    elem.css('margin-bottom', param === '*' ? elem.data('original-margin-bottom') : param);
+  });
+}
+
+function changeContrast(param) {
+  const id = 'link-contrast-accesa';
+  const documentElementStyle = document.documentElement.style;
+
+  let url = window.chrome.extension.getURL('contrast.css');
+
+  switch (param) {
+  case 'blackWhite':
+    documentElementStyle.setProperty('--color', 'black');
+    documentElementStyle.setProperty('--background', 'white');
+    documentElementStyle.setProperty('--border', 'silver');
+    break;
+  case 'blackYellow':
+    documentElementStyle.setProperty('--color', 'black');
+    documentElementStyle.setProperty('--background', 'yellow');
+    documentElementStyle.setProperty('--border', 'gold');
+    break;
+  case 'yellowBlack':
+    documentElementStyle.setProperty('--color', 'yellow');
+    documentElementStyle.setProperty('--background', 'black');
+    documentElementStyle.setProperty('--border', '#333');
+    break;
+  case 'whiteBlack':
+    documentElementStyle.setProperty('--color', 'white');
+    documentElementStyle.setProperty('--background', 'black');
+    documentElementStyle.setProperty('--border', 'grey');
+    break;
+  default:
+    documentElementStyle.removeProperty('--color');
+    documentElementStyle.removeProperty('--background');
+    documentElementStyle.removeProperty('--border');
+    url = '';
+    break;
+  }
+
+  if (url) {
+    let style = document.getElementById(id);
+    if (!style) {
+      style = document.createElement('link');
+      style.rel = 'stylesheet';
+      style.id = id;
+      style.type = 'text/css';
+      (document.head || document.documentElement).appendChild(style);
+    }
+    style.href = url;
+  } else {
+    $('#' + id).prop('disabled', true);
+    $('#' + id).remove();
+  }
 }
 
 function showImages() {
@@ -55,77 +189,189 @@ function hideImages() {
 }
 
 function showAds() {
-
+  $("ins[class='adsbygoogle'],a[href*='ads.e-planning.net'],a[href*='adclick.g.doubleclick.net'],div[id*='google_ads_iframe']").show();
 }
 
 function hideAds() {
-  /* $("ins[class='adsbygoogle'],a[href*='ads.e-planning.net'],a[href*='adclick.g.doubleclick.net'],div[id*='google_ads_iframe']").each(function () {
-     const elem = $(this);
-     if (valueSetting === 'false')
-       elem.css({ 'display': 'none', 'visibility': 'hidden' });
-     else
-       elem.css({ 'display': elem.data('defaultDisplay'), 'visibility': elem.data('defaultVisibility') });
-   });*/
+  $("ins[class='adsbygoogle'],a[href*='ads.e-planning.net'],a[href*='adclick.g.doubleclick.net'],div[id*='google_ads_iframe']").hide();
 }
 
 function showSocialNetworks() {
-  localStorage.setItem('input.name.hideSocialNetworks', false);
+  //localStorage.setItem('input.name.hideSocialNetworks', false);
   $("a[href!='']").filter(function() {
     let hrefValue = $(this).attr('href');
-    if (hrefValue) {
-      hrefValue = hrefValue.toLowerCase();
-      return socialNetworks.some(function(socialNetwork) {
-        return hrefValue.includes(socialNetwork);
-      });
-    } else {
-      return false;
-    }
+    hrefValue = hrefValue.toLowerCase();
+    return socialNetworks.some(function(socialNetwork) {
+      return hrefValue.includes(socialNetwork);
+    });
   }).show();
 }
 
 function hideSocialNetworks() {
-  localStorage.setItem('input.name.hideSocialNetworks', true);
+  //localStorage.setItem('input.name.hideSocialNetworks', true);
   $("a[href!='']").filter(function() {
     let hrefValue = $(this).attr('href');
-    if (hrefValue) {
-      hrefValue = hrefValue.toLowerCase();
-      return socialNetworks.some(function(socialNetwork) {
-        return hrefValue.includes(socialNetwork);
-      });
-    } else {
-      return false;
-    }
+    hrefValue = hrefValue.toLowerCase();
+    return socialNetworks.some(function(socialNetwork) {
+      return hrefValue.includes(socialNetwork);
+    });
   }).hide();
 }
 
-function changeAlign() {
-  /*const elements = $('*');
-  elements.removeClass('text-align-aw');
-  if (valueSetting !== 'none') {
-    document.documentElement.style.setProperty('--textAlign', valueSetting);
-    elements.addClass('text-align-aw');
-  }*/
+function enableDisableAccesibilityHTML(param) {
+  param ? enableAccesibilityHTML() : disableAccesibilityHTML();
 }
 
-function changeFont() {
+function enableAccesibilityHTML() {
 
+  // Agregar roles ARIA a elementos específicos
+  $('nav:not([role]), nav[role=""]').attr('role', 'navigation');
+  $('header:not([role]), header[role=""]').attr('role', 'banner');
+  $('footer:not([role]), footer[role=""]').attr('role', 'contentinfo');
+  $('main:not([role]), main[role=""]').attr('role', 'main');
+  $('aside:not([role]), aside[role=""]').attr('role', 'complementary');
+  $('section:not([role]), section[role=""]').attr('role', 'region');
+  $('form:not([role]), form[role=""]').attr('role', 'form');
+  $('article:not([role]), article[role=""]').attr('role', 'article');
+  $('button:not([role]), button[role=""]').attr('role', 'button');
+  $('ul:not([role]), ul[role=""]').attr('role', 'list');
+  $('ol:not([role]), ol[role=""]').attr('role', 'list');
+  $('li:not([role]), li[role=""]').attr('role', 'listitem');
+  $('table:not([role]), table[role=""]').attr('role', 'table');
+  $('th:not([role]), th[role=""]').attr('role', 'columnheader');
+  $('td:not([role]), td[role=""]').attr('role', 'cell');
+  $('tr:not([role]), tr[role=""]').attr('role', 'row');
+  $('img:not([role]), img[role=""]').attr('role', 'img');
+  $('a:not([role]), a[role=""]').attr('role', 'link');
+
+  // Agregar atributos ARIA a elementos con identificadores específicos
+  $('[id!=""]').each(function() {
+    const id = $(this).attr('id');
+    if (/captcha/i.test(id)) { // Verifica si el id contiene la palabra "captcha"
+      const ariaLabel = $(this).attr('aria-label');
+      if (!ariaLabel) { // Verifica si el aria-label está vacío o no definido
+        $(this).attr('aria-label', 'captcha');
+      }
+    }
+  });
+
+  // Agregar atributos ARIA a elementos con clases específicas
+  $('[class!=""]').each(function() {
+    const clazz = $(this).attr('class');
+    if (/captcha/i.test(clazz)) { // Verifica si el class contiene la palabra "captcha"
+      const ariaLabel = $(this).attr('aria-label');
+      if (!ariaLabel) { // Verifica si el aria-label está vacío o no definido
+        $(this).attr('aria-label', 'captcha');
+      }
+    }
+  });
+
+  // Agregar atributos ARIA a elementos de formulario
+  $('input[id!=""], textarea[id!=""], select[id!=""]').each(function() {
+    const id = $(this).attr('id');
+    const label = $(`label[for="${id}"]`);
+    if (label.length > 0) {
+      const ariaLabelledBy = $(this).attr('aria-labelledby');
+      if (!ariaLabelledBy) { // Verifica si el aria-labelledby está vacío o no definido
+        $(this).attr('aria-labelledby', id);
+      }
+    }
+  });
+
+  // Agregar atributos ARIA a botones y enlaces
+  $('button,a').each(function() {
+    const text = $(this).text().trim();
+    if (text) {
+      const ariaLabel = $(this).attr('aria-label');
+      if (!ariaLabel) { // Verifica si el aria-label está vacío o no definido
+        $(this).attr('aria-label', text);
+      }
+    }
+  });
+
+  // Agregar atributos ARIA a imágenes
+  $('img').each(function() {
+    const alt = $(this).attr('alt');
+    if (!alt) {
+      $(this).attr('aria-hidden', 'true');
+    }
+  });
+
+  // Agregar atributos ARIA a elementos interactivos
+  $('[tabindex]').each(function() {
+    const role = $(this).attr('role');
+    if (!role) { // Verifica si el role está vacío o no definido
+      $(this).attr('role', 'button');
+    }
+  });
+
+  // Agregar atributos ARIA a elementos con estados
+  $('[aria-expanded]').each(function() {
+    const expanded = $(this).attr('aria-expanded');
+    if (expanded === 'true') {
+      $(this).attr('aria-expanded', 'true');
+    } else {
+      $(this).attr('aria-expanded', 'false');
+    }
+  });
+
+  // Agregar atributos ARIA a elementos con descripciones
+  /* A efectos practicos esta logica no hace nada
+  $('[aria-describedby]').each(function() {
+    const describedby = $(this).attr('aria-describedby');
+    if (describedby) {
+      $(this).attr('aria-describedby', describedby);
+    }
+  });
+  */
+
+  // Agregar aria-live a elementos dinámicos
+  $('[data-dynamic]').each(function() {
+    const ariaLive = $(this).attr('aria-live');
+    if (!ariaLive) { // Verifica si el aria-live está vacío o no definido
+      $(this).attr('aria-live', 'polite');
+    }
+  });
+
+  // Agregar aria-controls a elementos que controlan otros elementos
+  $('[data-controls]').each(function() {
+    const controls = $(this).attr('data-controls');
+    if (controls) {
+      const ariaControls = $(this).attr('aria-controls');
+      if (!ariaControls) { // Verifica si el aria-controls está vacío o no definido
+        $(this).attr('aria-controls', controls);
+      }
+    }
+  });
+
+  // Agregar aria-haspopup a elementos que abren menús o diálogos
+  $('[data-has-popup]').each(function() {
+    const ariaHasPopup = $(this).attr('aria-haspopup');
+    if (!ariaHasPopup) { // Verifica si el aria-haspopup está vacío o no definido
+      $(this).attr('aria-haspopup', 'true');
+    }
+  });
+
+  // Agregar aria-current a elementos que representan el estado actual
+  $('.current').each(function() {
+    const ariaCurrent = $(this).attr('aria-current');
+    if (!ariaCurrent) { // Verifica si el aria-current está vacío o no definido
+      $(this).attr('aria-current', 'page');
+    }
+  });
+
+  // Agregar role="alert" a elementos que muestran mensajes importantes
+  $('.alert').each(function() {
+    const role = $(this).attr('role');
+    if (!role) { // Verifica si el role está vacío o no definido
+      $(this).attr('role', 'alert');
+    }
+  });
 }
 
-function changeFontSize() {
-
+function disableAccesibilityHTML() {
 }
 
-function changeLineSpacing() {
-
-}
-
-function changeParagraphSpacing() {
-
-}
-
-function changeContrast() {
-
-}
 
 /*
 export default {
@@ -146,5 +392,14 @@ export default {
 
 // Mapeo de acciones a funciones
 export const actionMap = {
-  'showImages': showHideImages
+  showImages: showHideImages,
+  showAds: showHideAds,
+  showSocialNetworks: showHideSocialNetworks,
+  changeTextAlign: changeTextAlign,
+  changeFont: changeFont,
+  changeFontSize: changeFontSize,
+  changeLineSpacing: changeLineSpacing,
+  changeParagraphSpacing: changeParagraphSpacing,
+  changeContrast: changeContrast,
+  enableAccesibilityHTML: enableDisableAccesibilityHTML
 };
